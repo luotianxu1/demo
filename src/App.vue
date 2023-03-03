ee8c8aa25328d145adaa4e4e8b2cfc759cd2128f
Luotianxu <1265062404@qq.com>
2023-03-03 17:13:31 +0800

    feat: scss

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 	<div>Hello Vue</div>
 	<el-button type="primary">按钮</el-button>
 </template>
-<script setup lang="ts">
-let a = ref(123)
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss"></style>
