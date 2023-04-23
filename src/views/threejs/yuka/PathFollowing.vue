4179b6c2af618258e71efa9dba3ad37862c25e3c
Luotianxu <1265062404@qq.com>
2023-04-23 16:03:33 +0800

    fix: 路径跟随

--- src/views/threejs/yuka/PathFollowing.vue
+++ src/views/threejs/yuka/PathFollowing.vue
@@ -30,6 +30,49 @@ light.position.set(10, 40, 10)
 light.castShadow = true
 scene.add(light)
 
+// 创建yuka的车辆
+const vehicle = new YUKA.Vehicle()
+vehicle.maxSpeed = 5
+
+// 创建yuka的路径
+const path = new YUKA.Path()
+path.add(new YUKA.Vector3(0, 0, 0))
+path.add(new YUKA.Vector3(0, 0, 10))
+path.add(new YUKA.Vector3(10, 0, 10))
+path.add(new YUKA.Vector3(10, 0, 0))
+path.add(new YUKA.Vector3(0, 0, 0))
+// 设置路径的循环模式
+path.loop = true
+
+// 将路径当前的位置设置为车辆的位置
+vehicle.position.copy(path.current())
+
+// 跟随路径的行为
+const followPathBehavior = new YUKA.FollowPathBehavior(path)
+vehicle.steering.add(followPathBehavior)
+
+// 保持在路径中行为
+const onPathBehavior = new YUKA.OnPathBehavior(path, 0.1)
+onPathBehavior.weight = 10
+vehicle.steering.add(onPathBehavior)
+
+// 创建对实体管理对象
+entityManager = new YUKA.EntityManager()
+entityManager.add(vehicle)
+
+showPathLine(path)
+function showPathLine(path: any) {
+	const positions = []
+	for (let i = 0; i < path._waypoints.length; i++) {
+		positions.push(path._waypoints[i].x, path._waypoints[i].y, path._waypoints[i].z)
+	}
+	const geometry = new THREE.BufferGeometry()
+	geometry.setAttribute("position", new THREE.Float32BufferAttribute(positions, 3))
+	const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
+	const line = new THREE.Line(geometry, material)
+	scene.add(line)
+}
+
 const webgl = ref()
 onMounted(() => {
 	init()
@@ -62,57 +105,21 @@ const init = () => {
 		car.children[0].scale.set(0.2, 0.2, 0.2)
 		scene.add(car)
 		// 设置车辆的渲染对象
-		vehicle.setRenderComponent(car, callback)
+		play(car)
 	})
-	function callback(entity: any, renderComponent: any) {
-		renderComponent.position.copy(entity.position)
-		renderComponent.quaternion.copy(entity.rotation)
-	}
-
-	// 创建yuka的车辆
-	const vehicle = new YUKA.Vehicle()
-	vehicle.maxSpeed = 5
-
-	// 创建yuka的路径
-	const path = new YUKA.Path()
-	path.add(new YUKA.Vector3(0, 0, 0))
-	path.add(new YUKA.Vector3(0, 0, 10))
-	path.add(new YUKA.Vector3(10, 0, 10))
-	path.add(new YUKA.Vector3(10, 0, 0))
-	path.add(new YUKA.Vector3(0, 0, 0))
-	// 设置路径的循环模式
-	path.loop = true
-
-	// 将路径当前的位置设置为车辆的位置
-	vehicle.position.copy(path.current())
-
-	// 跟随路径的行为
-	const followPathBehavior = new YUKA.FollowPathBehavior(path)
-	vehicle.steering.add(followPathBehavior)
-
-	// 保持在路径中行为
-	const onPathBehavior = new YUKA.OnPathBehavior(path, 0.1)
-	onPathBehavior.weight = 10
-	vehicle.steering.add(onPathBehavior)
-
-	// 创建对实体管理对象
-	entityManager = new YUKA.EntityManager()
-	entityManager.add(vehicle)
-
-	showPathLine(path)
-	function showPathLine(path: any) {
-		const positions = []
-		for (let i = 0; i < path._waypoints.length; i++) {
-			positions.push(path._waypoints[i].x, path._waypoints[i].y, path._waypoints[i].z)
-		}
-		const geometry = new THREE.BufferGeometry()
-		geometry.setAttribute("position", new THREE.Float32BufferAttribute(positions, 3))
-		const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
-		const line = new THREE.Line(geometry, material)
-		scene.add(line)
-	}
 
 	animate()
+}
+
+const play = (car: THREE.Group) => {
+	console.log(123)
+
+	vehicle.setRenderComponent(car, callback)
+}
+
+function callback(entity: any, renderComponent: any) {
+	renderComponent.position.copy(entity.position)
+	renderComponent.quaternion.copy(entity.rotation)
 }
 
 const time = new YUKA.Time()
