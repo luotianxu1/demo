ce414fa7ff13f3c2258b0073ebc3e8cf57d08833
Luotianxu <1265062404@qq.com>
2023-05-04 19:57:12 +0800

    fix: konva中icon

--- src/views/canvas/konva/editor/utils/consts.ts
+++ src/views/canvas/konva/editor/utils/consts.ts
@@ -22,18 +22,18 @@ export const componentShapeType: IComponentShapeTypeMap = {
 
 export const componentShapeList: IComponentShape[] = [
 	{ name: "矩形", icon: "icon-juxing", type: componentShapeType.RECT },
-	{ name: "平行四边形", icon: "icon-tx-pinghangsibianxing", type: componentShapeType.PARALLELOGRAM },
-	{ name: "菱形", icon: "icon-tubiao", type: componentShapeType.RHOMBUS },
-	{ name: "直线", icon: "icon-line", type: componentShapeType.LINE },
-	{ name: "箭头", icon: "icon-xiaoyoujiantou", type: componentShapeType.ARROWS },
+	{ name: "平行四边形", icon: "icon-pinghangsibianxing", type: componentShapeType.PARALLELOGRAM },
+	{ name: "菱形", icon: "icon-lingxing", type: componentShapeType.RHOMBUS },
+	{ name: "直线", icon: "icon-zhixian", type: componentShapeType.LINE },
+	{ name: "箭头", icon: "icon-fanhuijiantou", type: componentShapeType.ARROWS },
 	{ name: "文本", icon: "icon-wenben", type: componentShapeType.TEXT },
-	{ name: "圆形", icon: "icon-yuanxingweixuanzhong", type: componentShapeType.CIRCLEL },
-	{ name: "椭圆", icon: "icon-tuoyuanxing", type: componentShapeType.OVAL },
+	{ name: "圆形", icon: "icon-radio", type: componentShapeType.CIRCLEL },
+	{ name: "椭圆", icon: "icon-tuoyuan", type: componentShapeType.OVAL },
 	{ name: "环形", icon: "icon-huanxingtu", type: componentShapeType.ANNULAR },
 	{ name: "五边形", icon: "icon-pentagon", type: componentShapeType.PENTAGON },
 	{ name: "六边形", icon: "icon-tx-liubianxing", type: componentShapeType.HEXAGON },
-	{ name: "五角星", icon: "icon-wujiaoxing", type: componentShapeType.FIVESTAR },
-	{ name: "梯形", icon: "icon-tx-tixing", type: componentShapeType.TRAPEZIUM },
+	{ name: "五角星", icon: "icon-ai64", type: componentShapeType.FIVESTAR },
+	{ name: "梯形", icon: "icon-tixing", type: componentShapeType.TRAPEZIUM },
 	{ name: "扇形", icon: "icon-shanxing", type: componentShapeType.FAN },
-	{ name: "心形", icon: "icon-heart", type: componentShapeType.HEART }
+	{ name: "心形", icon: "icon-hear", type: componentShapeType.HEART }
 ]
